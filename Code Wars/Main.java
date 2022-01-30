435d06ec979c23b12c903d4ec55cf7a803544af5
D-Sava <70351283+D-Sava@users.noreply.github.com>
2022-01-30 22:28:02 +0100

    Add files via upload

---
+++ Code Wars/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Exercises > 8 kyu: Beginner Series #1 School Paperwork TODO#001 - DONE.
+        System.out.println("EXERCISE #1 - Output:\n");
+        System.out.println(Exercise001.paperWork(5,5));
+        System.out.println("\nSuccess!\n");
+
+        //Exercises > 8 kyu: Opposites Attract TODO#002 - DONE.
+        System.out.println("EXERCISE #2 - Output:\n");
+        System.out.println(Exercise002.isLove(1,4));
+        System.out.println("\nSuccess!\n");
+
+        //Exercises > 8 kyu: Remove exclamation marks TODO#003 - DONE.
+        System.out.println("EXERCISE #3 - Output:\n");
+        System.out.println(Exercise003.removeExclamationMarks("aisjd! iasjdi! asjd"));
+        System.out.println("\nSuccess!\n");
+
+        //Exercises > 8 kyu: Double Char TODO#004 - DONE.
+        System.out.println("EXERCISE #4 - Output:\n");
+        System.out.println(Exercise004.doubleChar("Car!"));
+        System.out.println("\nSuccess!\n");
+
+
+    }
+}
